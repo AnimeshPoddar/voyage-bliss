311bda29a49cc8e0064ca1332a29355d0b77fa30
Animesh Poddar <animesh123soni@gmail.com>
2024-09-03 15:11:51 +0530

    feat: created a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<>kakak</>} />
+          <Route path="/about-us" exact element={<></>} />
+          <Route path="/services" exact element={<></>} />
+          <Route path="/contact-us" exact element={<></>} />
+          <Route path="/sign-up" exact element={<></>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./navbar.style.css";
+import { Link } from "react-router-dom";
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+        <h1 className="site-icon">Voyage Bliss</h1>
+
+      <div className="menu-icons">
+        <i
+          onClick={() => setDrawerOpen(!drawerOpen)}
+          className={
+            !drawerOpen ? "fa-solid fa-bars-staggered" : "fa-solid fa-xmark"
+          }
+        ></i>
+      </div>
+
+      <ul className={drawerOpen ? "navbar-links active" : "navbar-links"}>
+        <li>
+          <Link className="link" to="/">
+            <i className="fa-solid fa-house-chimney"></i>Home
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/about-us">
+            <i className="fa-solid fa-circle-info"></i>About Us
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/services">
+            <i className="fa-solid fa-briefcase"></i>Services
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/contact-us">
+            <i className="fa-regular fa-id-badge"></i>Contact Us
+          </Link>
+        </li>
+        <li>
+          <Link className="signup-link" to="/sign-up">
+            <i className="fa-regular fa-id-badge"></i>Sign Up
+          </Link>
+        </li>
+        <button className="sign-up-button">Sign Up</button>
+      </ul>
+    </nav>
+  );
 };
